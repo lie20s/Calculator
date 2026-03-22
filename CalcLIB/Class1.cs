dd5d514fdb6430bce185626be39798b2894aedc0
lie20s <belyanin-03@bk.ru>
2026-03-22 18:00:40 +0500

    Добавьте файлы проекта.

---
+++ CalcLIB/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace CalcLIB
+{
+    public class Class1
+    {
+        public static dynamic Execute(dynamic first, char op, dynamic second) 
+        {
+            dynamic result = 0;
+            switch (op)
+            {
+                case '+':
+                    result = first + second; break;
+                case '-':
+                    result = first - second; break;
+                case '*':
+                    result = first * second; break;
+                case '/':
+                    if (second == 0)
+                        throw new DivideByZeroException("Деление на 0");
+                    result = first / second; break;
+                case '^':
+                    result = Math.Pow(first, second); break;
+                default:
+                    result = 0; break;
+            }
+            return result;
+        }
+
+        public double Execute(double firstNumber, double secondNumber, char currentOperator)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CalcLIB;
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        private double currentValue = 0;
+        private string currentOperator = "";
+        private bool isNewEntry = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtDisplay.Text = "0";
+        }
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string number = button.Content.ToString();
+
+            if (isNewEntry)
+            {
+                txtDisplay.Text = number;
+                isNewEntry = false;
+            }
+            else
+            {
+                if (txtDisplay.Text == "0")
+                    txtDisplay.Text = number;
+                else
+                    txtDisplay.Text += number;
+            }
+        }
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string op = button.Content.ToString();
+
+            if (!isNewEntry && currentOperator != "")
+            {
+                Calculate();
+            }
+
+            currentValue = double.Parse(txtDisplay.Text);
+            currentOperator = op;
+            isNewEntry = true;
+        }
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentOperator != "")
+            {
+                Calculate();
+                currentOperator = "";
+                isNewEntry = true;
+            }
+        }
+        private void Calculate()
+        {
+            try
+            {
+                double secondNumber = double.Parse(txtDisplay.Text);
+                double result = 0;
+                char operation = currentOperator[0];
+                result = Class1.Execute(currentValue, operation, secondNumber);
+
+                txtDisplay.Text = result.ToString();
+                currentValue = result;
+            }
+            catch (DivideByZeroException)
+            {
+                txtDisplay.Text = "Ошибка: деление на 0";
+                currentValue = 0;
+                currentOperator = "";
+                isNewEntry = true;
+            }
+            catch (Exception)
+            {
+                txtDisplay.Text = "Ошибка";
+                currentValue = 0;
+                currentOperator = "";
+                isNewEntry = true;
+            }
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text = "0";
+            currentValue = 0;
+            currentOperator = "";
+            isNewEntry = true;
+        }
+        private void Decimal_Click(object sender, RoutedEventArgs e)
+        {
+            if (isNewEntry)
+            {
+                txtDisplay.Text = "0,";
+                isNewEntry = false;
+            }
+            else if (!txtDisplay.Text.Contains(","))
+            {
+                txtDisplay.Text += ",";
+            }
+        }
+    }
+}
